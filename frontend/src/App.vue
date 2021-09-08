0e41ca10fc13e3a12bc5aba878ccdd8c62f4dc13
BiliSnod <mxmgznnc@gmail.com>
2021-09-08 03:49:44 +0200

    MOD[frontend] editing router for notes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,53 +1,77 @@
 <template>
-  <nav id="nav">
-    <router-link to="/">Accueil</router-link>
-    <router-link to="/signup">Inscription</router-link>
-    <router-link to="/login">Connexion</router-link>
-  </nav>
-  <router-view />
+    <div id="app">
+
+        <nav class="nav">
+            <ul>
+                <li><router-link to="/">Accueil</router-link></li>
+                <li><router-link to="/signup">Inscription</router-link></li>
+                <li><router-link to="/login">Connexion</router-link></li>
+            </ul>
+            <ul>
+                <li><router-link to="/notes">Notes</router-link></li>
+                <li><router-link to="/new">Postez</router-link></li>
+            </ul>
+        </nav>
+
+        <div>
+            <router-view />
+        </div>
+
+    </div>
 </template>
+
+<script>
+export default {
+    name: "app"
+};
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #312c50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #312c50;
 }
 
-#nav {
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
-  padding: 30px;
+.nav {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    padding: 30px;
 
-  @media screen and (min-width: 640px) {
-    justify-content: flex-end;
-  }
-
-  a {
-    background-color: #fcfcfc;
-    border: 3px solid #eee;
-    border-radius: 15px;
-    color: #312c50;
-    font-weight: bold;
-    margin: 5px;
-    min-width: 100px;
-    padding: 10px;
-    text-decoration: none;
-    transition: all 200ms ease-out;
-
-    &:hover {
-      background-color: #fff;
-      border: 3px solid #ddd;
-      color: #000;
+    @media screen and (min-width: 640px) {
+        justify-content: flex-end;
     }
 
-    &.router-link-exact-active {
-      color: #ad1f01;
-      background-color: #fff7f5;
+    li {
+        display: inline-block;
     }
-  }
+
+    a {
+        background-color: #fcfcfc;
+        border: 3px solid #eee;
+        border-radius: 15px;
+        color: #312c50;
+        display: block;
+        font-weight: bold;
+        margin: 5px;
+        padding: 10px;
+        text-decoration: none;
+        transition: all 200ms ease-out;
+        width: 100px;
+
+        &:hover {
+            background-color: #fff;
+            border: 3px solid #ddd;
+            color: #000;
+        }
+
+        &.router-link-exact-active {
+            color: #ad1f01;
+            background-color: #fff7f5;
+        }
+    }
 }
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,28 +1,49 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router"
+import Home from "../views/Home.vue"
 
-/* --- Defining routes for each linked page --- */
+
+/* --- Defining routes for each linked page [o] --- */
 const routes = [
-  {
-    path: '/',
-    name: 'Accueil',
-    component: Home
-  },
-  {
-    path: '/signup',
-    name: 'Inscription',
-    component: () => import(/* webpackChunkName: "signup" */ '../views/Signup.vue')
-  },
-  {
-    path: '/login',
-    name: 'Connexion',
-    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
-  }
-]
+    {
+        path: "/",
+        name: "Accueil",
+        component: Home
+    },
+    {
+        path: "/signup",
+        name: "Inscription",
+        component: () => import(/* webpackChunkName: "signup" */ "../views/Signup.vue")
+    },
+    {
+        path: "/login",
+        name: "Connexion",
+        component: () => import(/* webpackChunkName: "login" */ "../views/Login.vue")
+    },
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    {
+        path: "/notes",  // "/",
+        // alias: "/notes",
+        name: "Notes",
+        component: () => import("../components/AllNotes")
+    },
+    {
+        path: "/notes/:id",
+        name: "Note",
+        component: () => import("../components/ViewNote")
+    },
+    {
+        path: "/new",
+        name: "Nouvelle note",
+        component: () => import("../components/NewNote")
+    }
+
+];
+/* --- Defining routes for each linked page [x] --- */
+
+
+const router = createRouter({  // switching from hash mode to history mode inside browser with HTML5 "History API"
+history: createWebHistory(),  // (process.env.BASE_URL),
+routes
 })
 
 export default router
